4f55906b049c07227d1635ab7a5f6048b7b311f0
avadhoot-athalye <avadhoot.athalye.49@gmail.com>
2016-12-16 05:43:15 +0530

    Added plugins and dependancies

---
+++ beam_webapp/src/main/java/com/beam/webapp/controller/PageController.java
@@ -0,0 +1,19 @@
+package com.beam.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value={"/","/home","/index","/default"})
+	
+	public ModelAndView index() {
+		
+		ModelAndView mv = new ModelAndView("index");
+		return mv;
+		
+	}
+
+}
